4b839f783a07a99563e09881f433427685cffc00
webbsta-28 <iwebb0462@gmail.com>
2026-03-15 18:15:20 +1000

    final

--- src/generate.py
+++ src/generate.py
@@ -1,9 +1,9 @@
 from blocks import markdown_to_html_node, extract_title
-from copy_static import copy_files_recursive
+
 import os
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as file:
@@ -15,18 +15,20 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(content_from_path)
     html_template = content_template_path.replace("{{ Title }}", title)
     html_template = html_template.replace(f"{{ Content }}", html_string)
+    html_template = html_template.replace('href="/',f'href="{{basepath}}')
+    html_template = html_template.replace('src="/',f'src="{{basepath}}')
     dest = os.path.dirname(dest_path)
     os.makedirs(dest, exist_ok=True)
     with open(dest_path, "w") as file:
         file.write(html_template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
         for filename in os.listdir(dir_path_content):
             from_path = os.path.join(dir_path_content, filename)
             dest_path = os.path.join(dest_dir_path, filename)
             print(f" * {from_path} -> {dest_path}")
             if os.path.isfile(from_path):
                 new_path = dest_path.replace("md", "html")
-                generate_page(from_path, template_path, new_path)
+                generate_page(from_path, template_path, new_path, basepath)
             else:
-                generate_pages_recursive(from_path, template_path, dest_path)
+                generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -4,16 +4,17 @@ from copy_static import copy_files_recursive
 import shutil
 from blocks import extract_title
 from generate import *
+import sys
 dir_path_static = "static"
-dir_path_public = "public"
+dir_path_public = "docs"
 
 def main():
-    
+    basepath = sys.argv or "/"
     if os.path.exists(dir_path_public):
         shutil.rmtree(dir_path_public)
 
    
     copy_files_recursive(dir_path_static, dir_path_public)
-    generate_pages_recursive("content", "template.html", "public")
+    generate_pages_recursive("content", "template.html", dir_path_public, basepath)
 
 main()
